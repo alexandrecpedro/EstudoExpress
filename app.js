98110d83087afd2fa6256ffa29ae8459d9c58039
Alexandre Pedro <alexandrecpedro@gmail.com>
2021-04-19 22:04:05 -0300

    criar usuários

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+// Usando express.json para pegar dados de formulário enviados pelo usuário
+// Indica para Express que usaremos os dados em JSON
+app.use(express.json());
 
 let usuarios = [
     {nome: "Alexandre", sobrenome: "Pedro"}
@@ -14,6 +17,21 @@ app.get("/inicio", (request, response) => {
 app.get("/usuarios", (request, response) => {
     // console.log(response);
     return response.json(usuarios);
+})
+
+// http://localhost:3000/usuarios
+app.post("/usuarios", (request, response) => {
+    const {nome, sobrenome} = request.body;
+    usuarios.push({nome, sobrenome});
+    return response.json({nome, sobrenome});
+})
+
+// http://localhost:3000/usuarios/alexandre
+app.put("/usuarios/:nome", (request, response) => {
+    const {nome} = request.params;
+    const {sobrenome} = request.body;
+    
+    return response.json({nome, sobrenome});
 })
 
 app.listen(3000, () => {
